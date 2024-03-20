60b5648a036dd14db3ef0526d257fb2a6e26b7ee
Matt Ouille <matt.ouille@protonmail.com>
2024-03-19 17:26:54 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/matt0x6f/athens-local
+
+go 1.22
+
+toolchain go1.22.1
+
+require github.com/matt0x6f/athens-test-repo v0.0.0-20240319235344-bd582fd524fa
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	athensTest "github.com/matt0x6f/athens-test-repo"
+)
+
+
+func main() {
+	fmt.Printf("%s", athensTest.HelloWorld())
+}
